549e56aded397bc51cd049601a8e08b229c0bb84
nan_kokuchu <nan_k@cloudseed.com>
2024-05-17 13:45:19 +0900

    基本APIを追加

---
+++ src/api/system/menu.js
@@ -0,0 +1,40 @@
+import request from '@/utils/request'
+
+// メニューに関するAPI
+const API_PATH = '/admin/system/sysMenu'
+
+export default {
+  // 全てのメニュー取得
+  findNodes() {
+    return request({
+      url: `${API_PATH}/getMenu`,
+      method: 'get'
+    })
+  },
+
+  // 削除
+  removeById(id) {
+    return request({
+      url: `${API_PATH}/removeMenu/${id}`,
+      method: 'delete'
+    })
+  },
+
+  // 保存
+  save(sysMenu) {
+    return request({
+      url: `${API_PATH}/saveMenu`,
+      method: 'post',
+      data: sysMenu
+    })
+  },
+
+  // 更新
+  updateById(sysMenu) {
+    return request({
+      url: `${API_PATH}/updateMenu`,
+      method: 'put',
+      data: sysMenu
+    })
+  }
+}
